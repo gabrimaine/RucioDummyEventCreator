a1f9ed52b0f1816249e1852d6aaa69e157b9f7b5
MAINETTI Gabriele <gabriele.mainetti@cc.in2p3.fr>
2024-10-17 15:27:03 +0200

    add test for event creator

--- tests/test_kafka_producer.py
+++ tests/test_kafka_producer.py
@@ -8,19 +8,18 @@ from lsst.rucioevents.kafka_producer import RucioKafkaProducer
 
 class TestRucioKafkaProducer(unittest.TestCase):
     def setUp(self):
-        """Configurazione comune per i test."""
+        """Setup the config."""
         self.topic = "test_topic"
         self.mock_producer = MagicMock()
         self.kafka_producer = RucioKafkaProducer(self.topic)
-        # Sostituisci il producer reale con il mock
         self.kafka_producer.producer = self.mock_producer
 
     def test_init(self):
-        """Verifica che l'inizializzazione funzioni correttamente."""
+        """Test init."""
         self.assertEqual(self.kafka_producer.topic, self.topic)
 
     def test_send_event_success(self):
-        """Verifica l'invio di un evento con successo."""
+        """Check event creation."""
         event = {"key": "test_key", "data": "test_data"}
         self.kafka_producer.send_event([event])
         self.mock_producer.produce.assert_called_once_with(
@@ -32,7 +31,7 @@ class TestRucioKafkaProducer(unittest.TestCase):
         self.mock_producer.flush.assert_called_once()
 
     def test_send_event_no_key(self):
-        """Verifica l'invio di un evento senza chiave)."""
+        """Test no key event."""
         event = {"data": "test_data"}
         self.kafka_producer.send_event([event])
         self.mock_producer.produce.assert_called_once()
@@ -48,7 +47,7 @@ class TestRucioKafkaProducer(unittest.TestCase):
         self.mock_producer.flush.assert_called_once()
 
     def test_send_event_multiple(self):
-        """Verifica l'invio di più eventi."""
+        """Chem many events production."""
         events = [
             {"key": "key1", "data": "data1"},
             {"key": "key2", "data": "data2"},
@@ -59,7 +58,7 @@ class TestRucioKafkaProducer(unittest.TestCase):
 
     @patch("lsst.rucioevents.kafka_producer.logger")
     def test_delivery_report_success(self, mock_logger):
-        """Verifica il caso di successo del delivery report."""
+        """Trying to test the callback."""
         events = [{"key": "test_key", "value": "test_value"}]
 
         def mock_produce(*args, **kwargs):
@@ -77,21 +76,6 @@ class TestRucioKafkaProducer(unittest.TestCase):
         mock_logger.info.assert_called_with(
             "Message: test_key successfully produced to Topic: test_topic Partition: [0] at offset 123"
         )
-
-        # def mock_produce_error(*args, **kwargs):
-        #    callback = kwargs.get("callback")
-        #    if callback:
-        #        mock_msg = MagicMock()
-        #        mock_msg.key.return_value = "test_key"
-        #        callback("Error", mock_msg)
-
-        # self.mock_producer.producer.produce.side_effect = mock_produce_error
-
-        # self.mock_producer.send_event(events)
-
-        # mock_logger.error.assert_called_with(
-        #    "Delivery failed for Message: test_key : Error"
-        # )
 
 
 class MemoryTester(lsst.utils.tests.MemoryTestCase):
